d0f4a940b90b4504942df11734b3698891ca60e4
Ilya <geil1997@mail.ru>
2024-10-02 09:16:54 +0300

    added all users endpoint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const sequelize = require("./db");
-const User = require("./models/User");
+const User = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
